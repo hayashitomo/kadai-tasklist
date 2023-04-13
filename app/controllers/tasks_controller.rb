c0c37d4f55e022467193ab511815a7e3fc9385e0
hayashi.tomoyuki <nakamaruhayashi@gmail.com>
2023-04-13 13:00:00 +0000

    kadai_0413

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,11 +12,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(message_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'task が正常に投稿されました'
-      redirect_to @message
+      redirect_to @task
     else
       flash.now[:danger] = 'task が投稿されませんでした'
       render :new
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
 
-    if @task.update(message_params)
+    if @task.update(task_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @task
     else
