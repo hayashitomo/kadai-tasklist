50143a7fe7d4a6494b3b8aafcac8da8ff0583316
hayashi.tomoyuki <nakamaruhayashi@gmail.com>
2023-05-15 10:35:45 +0000

    20230515_kadai

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movie
+(1..100).each do |number|
+  Task.create(status: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+end
